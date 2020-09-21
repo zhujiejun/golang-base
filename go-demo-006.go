be8c143dc34093104c5b058dc2acacfe902b7074
zhujiejun <2383322468@qq.com>
2020-09-21 13:07:30 +0800

    commit

--- go-demo-006.go
+++ go-demo-006.go
@@ -2,16 +2,35 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-func main() {
+func calculate(max int) {
+	var lock sync.Mutex
 	result := 0
 	start := time.Now().UnixNano()
-	for i := 1; i <= 1234567890123; i++ {
+	for i := 1; i <= max; i++ {
+		lock.Lock()
 		result += i
+		lock.Unlock()
 	}
 	end := time.Now().UnixNano()
-	//fmt.Println("the result is ", result, "and total time consumption is ", (end-start)/1e9, " seconds.")
 	fmt.Printf("the result is %d and total time consumption is %d seconds.\n", result, ((end - start) / 1e9))
+}
+
+func main() {
+	//input := bufio.NewScanner(os.Stdin)
+	//for input.Scan() {
+	//	in := input.Text()
+	//	//fmt.Println("in:", in)
+	//	calculate(
+	//}
+	var input int
+	fmt.Println("please input the max number:")
+	fmt.Scanln(&input)
+	for k := 1; k <= 5; k++ {
+		go calculate(input)
+	}
+	time.Sleep(time.Second)
 }
