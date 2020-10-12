76644af5b0e3f0051a52f109eb9f9f5a4826bcda
zhujiejun <2383322468@qq.com>
2020-10-12 09:50:47 +0800

    commit

--- go-demo-003.go
+++ go-demo-003.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func calculate(z float64) float64 {
+func cal(z float64) float64 {
 	n := 1
 	m := 3
 	t := 2.0
@@ -20,6 +20,6 @@ func calculate(z float64) float64 {
 }
 
 func main() {
-	sum := calculate(1e-1234567890)
+	sum := cal(1e-1234567890)
 	fmt.Println(sum)
 }
--- main/Server.go
+++ main/Server.go
@@ -32,7 +32,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func counter(w http.ResponseWriter, r *http.Request) {
 	lock.Lock()
-	fmt.Fprintf(w, "Count %d\n", count)
+	_, _ = fmt.Fprintf(w, "Count %d\n", count)
 	lock.Unlock()
 }
 
@@ -53,11 +53,11 @@ func lissajous(out io.Writer) {
 		for t := 0.0; t < cycles*2*math.Pi; t += resource {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), 25)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), 30)
 		}
 		phase += 0.1
 		anim.Delay = append(anim.Delay, delay)
 		anim.Image = append(anim.Image, img)
 	}
-	gif.EncodeAll(out, &anim)
+	_ = gif.EncodeAll(out, &anim)
 }
